8d8b5a3937b8910a84d8e81064e41dc42d949ab7
mlzzÂ <miguel.abad@gft.com>
2024-07-09 09:49:49 +0200

    add basic routing and project folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './views'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/game' element={<Home />} />
+        <Route path='*' element={<Navigate to='/' replace />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css'
+
+function Home () {
+  return (
+    <>
+      <h1>Squid Game (Statues)</h1>
+    </>
+  )
+}
+
+export default Home
