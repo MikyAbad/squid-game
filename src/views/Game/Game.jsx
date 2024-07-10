8440b131f2b3f73cf407a67e14681da3262b1c29
mlzzÂ <miguel.abad@gft.com>
2024-07-10 10:23:20 +0200

    minor cosmetic changes in Game.js

--- src/views/Game/Game.jsx
+++ src/views/Game/Game.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getCurrentUser, saveScore, startLightCycle, handleUserClick, stopLightCycle } from '../../services'
 import { FaPlayCircle, FaStopCircle } from 'react-icons/fa'
+import { GiOctopus } from 'react-icons/gi'
 import './Game.css'
 
 export function Game () {
@@ -29,22 +30,27 @@ export function Game () {
   const handleExit = () => { navigate('/') }
 
   return (
-    <section className='game-container'>
-      <header className='game-header'>
-        <h2>Player: {user.name}</h2>
-        <p>Max Score: {user.maxScore}</p>
-      </header>
-      <main className='game-main'>
-        <div className='light'>
-          {light === 'red' ? <FaStopCircle size={50} color='red' /> : <FaPlayCircle size={50} color='green' />}
-        </div>
-        <p className='current-score'>Score: {user.score}</p>
-        <div className='buttons-container'>
-          <button className='game-button' onClick={() => handleButtonPress('left')}>Left</button>
-          <button className='game-button' onClick={() => handleButtonPress('right')}>Right</button>
-        </div>
-        <button className='exit-button' onClick={handleExit}>Exit</button>
-      </main>
-    </section>
+    <main>
+      <section className='logo-header'>
+        <GiOctopus className='logo' size={75} color='#14549c' />
+      </section>
+      <section className='game-container'>
+        <header className='game-header'>
+          <h2>Player: {user.name}</h2>
+          <p>Max Score: {user.maxScore}</p>
+        </header>
+        <main className='game-main'>
+          <div className='light'>
+            {light === 'red' ? <FaStopCircle size={75} color='red' /> : <FaPlayCircle size={75} color='green' />}
+          </div>
+          <p className='current-score'>Score: {user.score}</p>
+          <div className='buttons-container'>
+            <button className='game-button' onClick={() => handleButtonPress('left')}>Left</button>
+            <button className='game-button' onClick={() => handleButtonPress('right')}>Right</button>
+          </div>
+          <button className='exit-button' onClick={handleExit}>Exit</button>
+        </main>
+      </section>
+    </main>
   )
 }
